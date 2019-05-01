73d8e61b0a2efcfa107bb86b46da8997a11648ee
Rusty Boot <nicholastuso@yahoo.com>
2019-04-30 19:43:42 -0700

    firebase initialize

---
+++ assets/javascript/web.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCumztwvFnlc-7_yRaGM5VwWlrT8JIdGYQ",
+    authDomain: "victoriabakery-e22a9.firebaseapp.com",
+    databaseURL: "https://victoriabakery-e22a9.firebaseio.com",
+    projectId: "victoriabakery-e22a9",
+    storageBucket: "victoriabakery-e22a9.appspot.com",
+    messagingSenderId: "978610075284"
+};
+firebase.initializeApp(config);
+var database=firebase.database();
