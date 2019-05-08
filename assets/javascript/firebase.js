ead202cef856a8e47d2109e3595cb7915c70cdd7
Rusty Boot <nicholastuso@yahoo.com>
2019-05-07 21:15:06 -0700

    Massive change

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCumztwvFnlc-7_yRaGM5VwWlrT8JIdGYQ",
+    authDomain: "victoriabakery-e22a9.firebaseapp.com",
+    databaseURL: "https://victoriabakery-e22a9.firebaseio.com",
+    projectId: "victoriabakery-e22a9",
+    storageBucket: "victoriabakery-e22a9.appspot.com",
+    messagingSenderId: "978610075284",
+    appId: "1:978610075284:web:be8bf21022ed92dd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //Reminder: Add "<script src="https://www.gstatic.com/firebasejs/6.0.1/firebase-app.js"></script>" to the desired html pages.
